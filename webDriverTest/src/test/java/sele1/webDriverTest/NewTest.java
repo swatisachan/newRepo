68f2bef98fb65a2e93bf4b58fe553ae44ca53ef2
swati sachan <swati sachan@DESKTOP-B7K6OT8>
2019-05-04 08:34:29 -0700

    Added project

---
+++ webDriverTest/src/test/java/sele1/webDriverTest/NewTest.java
@@ -0,0 +1,13 @@
+package sele1.webDriverTest;
+
+import static org.testng.Assert.assertNotEquals;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  assertNotEquals("swati", "swati");
+  }
+}
---
+++ webDriverTest/src/test/java/sele1/webDriverTest/NewTest1.java
@@ -0,0 +1,13 @@
+package sele1.webDriverTest;
+
+import static org.testng.Assert.assertNotEquals;
+
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+  @Test
+  public void f() {
+	  
+	  assertNotEquals("swat", "swat");
+  }
+}
